d53e06c97743be08dbc675aaef8661c84a5c7694
Rodrigo C. Garcia <rodiza082003@hotmail.com>
2025-09-19 23:26:43 -0300

    Adicionando input

--- src/template/Pilha.java
+++ src/template/Pilha.java
@@ -5,18 +5,30 @@ import br.com.davidbuzatto.jsge.core.utils.CoreUtils;
 import br.com.davidbuzatto.jsge.core.utils.DrawingUtils;
 import br.com.davidbuzatto.jsge.geom.Rectangle;
 import br.com.davidbuzatto.jsge.image.Image;
+import br.com.davidbuzatto.jsge.imgui.GuiButton;
+import br.com.davidbuzatto.jsge.imgui.GuiInputDialog;
+import java.util.List;
 
 public class Pilha extends EngineFrame {
     
-    private Image logo;
-    private int[] elementos;
+    private int[] elementosPilha;
+    private int topo;
+    
+    private GuiInputDialog inputDialog;
+    private GuiButton inputButton;
+    private GuiButton inputConfirm;
+    
+    
+    int x;
+    int y;
+    int contador;
     
     public Pilha() {
         
         super(
             800,                 // largura                      / width
             600,                 // algura                       / height
-            "Projeto Ordenação", // título                       / title
+            "Pilha",             // título                       / title
             60,                  // quadros por segundo desejado / target FPS
             true,                // suavização                   / antialiasing
             false,               // redimensionável              / resizable
@@ -30,16 +42,46 @@ public class Pilha extends EngineFrame {
     
     @Override
     public void create() {
+        x = 350;
+        y = 200;
+        contador = 0;
+        elementosPilha = new int[10];
         
+        inputButton = new GuiButton(x, y, 100, 100, "teste", this);
+        inputDialog = new GuiInputDialog("Inserir dados", 
+                "Insira um elemento na pilha", "Cancelar", true, this);
+        
+      
     }
 
     @Override
     public void update( double delta ) {
+        inputButton.update(delta);
+        inputDialog.update(delta);
+        
+        //abrir a aba de input
+        if(inputButton.isMousePressed()){
+            inputDialog.show();
+        }
+        
+        //fechar a aba de input
+        if ( inputDialog.isCloseButtonPressed() ) {
+            inputDialog.hide();
+        }
+        
+        //push
+        if(inputDialog.isOkButtonPressed() || inputDialog.isEnterKeyPressed()){
+            elementosPilha[contador] = Integer.parseInt(inputDialog.getValue());
+            contador++;
+            topo = Integer.parseInt(inputDialog.getValue());
+            inputDialog.hide();
+        }
     }
     
     @Override
     public void draw() {  
-        
+        inputButton.draw();
+        inputDialog.draw();
     }
     
     public static void main( String[] args ) {
