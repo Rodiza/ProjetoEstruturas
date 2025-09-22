1088e417f85c5fe40d3bb75548d937308c69bfee
Rodrigo C. Garcia <rodiza082003@hotmail.com>
2025-09-22 02:14:12 -0300

    Inserção de numeros primitiva

--- src/template/Pilha.java
+++ src/template/Pilha.java
@@ -7,6 +7,7 @@ import br.com.davidbuzatto.jsge.geom.Rectangle;
 import br.com.davidbuzatto.jsge.image.Image;
 import br.com.davidbuzatto.jsge.imgui.GuiButton;
 import br.com.davidbuzatto.jsge.imgui.GuiInputDialog;
+import br.com.davidbuzatto.jsge.math.Vector2;
 import java.util.List;
 
 public class Pilha extends EngineFrame {
@@ -19,11 +20,12 @@ public class Pilha extends EngineFrame {
     private GuiButton pop;
     
     private Rectangle contornoPilha;
+    private Vector2 ponto;
     
     
-    int x;
-    int y;
-    int contador;
+    private int x;
+    private int y;
+    private int contador;
     
     public Pilha() {
         
@@ -49,6 +51,7 @@ public class Pilha extends EngineFrame {
         contador = 0;
         elementosPilha = new int[10];
         
+        ponto = new Vector2(x, y);
         push = new GuiButton(x, y, 100, 70, "Push", this);
         pop = new GuiButton(x, y + 100, 100, 70, "Pop", this);
         inputPush = new GuiInputDialog("Inserir dados", 
@@ -69,7 +72,7 @@ public class Pilha extends EngineFrame {
         if(push.isMousePressed()){
             inputPush.show();
         }
-        
+       
         //fechar a aba de input
         if ( inputPush.isCloseButtonPressed() ) {
             inputPush.hide();
@@ -77,10 +80,17 @@ public class Pilha extends EngineFrame {
         
         //push
         if(inputPush.isOkButtonPressed() || inputPush.isEnterKeyPressed()){
-            elementosPilha[contador] = Integer.parseInt(inputPush.getValue());
-            contador++;
-            topo = Integer.parseInt(inputPush.getValue());
-            inputPush.hide();
+            if(ehInt(inputPush.getValue())){
+                elementosPilha[contador] = Integer.parseInt(inputPush.getValue());
+
+                topo = Integer.parseInt(inputPush.getValue());
+                inputPush.hide();
+
+                ponto.y += 20;
+                contador++;
+            }else{
+                inputPush.setText("Precisa ser um numero");
+            }
         }
         
         //pop
@@ -93,10 +103,49 @@ public class Pilha extends EngineFrame {
     public void draw() {  
         push.draw();
         pop.draw();
-        contornoPilha.draw(this, BLACK);
+        desenharPilha(contornoPilha, elementosPilha);
         inputPush.draw();
         
+        
+            
+        
     }
+    
+    //Usado para chechar se a entrada é um int
+    private boolean ehInt(String string){
+        try {
+            Integer.parseInt(string);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    
+    //Desenha uma pilha de tamanho fixo 10
+    public void desenharPilha(Rectangle contorno, int[] array){
+        contorno.draw(this, BLACK);
+        double tamanhoElemento = contorno.height / elementosPilha.length;
+        
+        for(int i = 0; i < contador; i++){
+            //Começar desenhando da base da pilha
+            double yPos = contorno.y + contorno.height - (i * tamanhoElemento);
+            double xPos = contorno.x + (contornoPilha.width / 2);
+            
+            drawText(Integer.toString(elementosPilha[i]), 
+                    new Vector2(xPos, yPos),
+                    20,
+                    BLACK
+                    );
+        }
+        
+        
+        
+        //Rectangle quadrado = new Rectangle(retangulo.x, retangulo.y, 
+        //        100, tamanhoElemento);
+        //quadrado.draw(this, BLACK);
+        
+    }
+    
     
     public static void main( String[] args ) {
         new Pilha();
